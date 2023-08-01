2568f74e7016b4c9c12ea7c1b9ac2e8627524a5b
chuckiedonald <101234522+chuckiedonald@users.noreply.github.com>
2023-08-01 18:30:49 +0100

    Update skillAnimat.js

--- public/javascript/skillAnimat.js
+++ public/javascript/skillAnimat.js
@@ -1,40 +1,32 @@
-function increaseValueOnScroll() { 
-     const skillBars = document.querySelectorAll('.skill-bar'); 
-     const duration = 3000; // 2 seconds 
-  
-     function increaseValue(skillBar, targetValue, targetDivClass) { 
-       let currentValue = 0; 
-       const targetDivs = document.querySelectorAll(".ch-wdt"); 
-  
-       const interval = setInterval(() => { 
-         if (currentValue <= targetValue) { 
-           skillBar.textContent = currentValue; 
-           targetDivs.forEach((div) => { 
-             div.style.width = currentValue + '%'; 
-           }); 
-           currentValue++; 
-         } else { 
-           clearInterval(interval); 
-         } 
-       }, duration / targetValue); 
-     } 
-  
-     const observer = new IntersectionObserver((entries) => { 
-       entries.forEach((entry) => { 
-         if (entry.isIntersecting) { 
-           const targetValue = parseInt(entry.target.textContent); 
-           const targetDivClass = entry.target.dataset.target; 
-           increaseValue(entry.target, targetValue, targetDivClass); 
-           observer.unobserve(entry.target); 
-         } 
-       }); 
-     }); 
-  
-     skillBars.forEach((skillBar) => { 
-       observer.observe(skillBar); 
-     }); 
-   } 
-  
-   document.addEventListener('DOMContentLoaded', () => { 
-     increaseValueOnScroll(); 
-   });
+function increaseValueOnScroll() {
+  const skillBars = document.querySelectorAll('.skill-bar');
+  const targetDivs = document.querySelectorAll('.ch-wdt');
+  const duration = 2000; // 2 seconds
+
+  function animateWidthChange(element, targetValue, duration) {
+    let currentValue = 0;
+    const incrementValue = targetValue / (duration / 1000);
+
+    const interval = setInterval(() => {
+      if (currentValue <= targetValue) {
+        element.textContent = currentValue;
+        element.style.width = (currentValue / targetValue) * 100 + '%';
+        currentValue += incrementValue;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000 / incrementValue);
+  }
+
+  skillBars.forEach((skillBar, index) => {
+    const targetValue = parseInt(skillBar.textContent);
+    animateWidthChange(skillBar, targetValue, duration);
+    if (targetDivs[index]) {
+      animateWidthChange(targetDivs[index], targetValue, duration);
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  increaseValueOnScroll();
+});
