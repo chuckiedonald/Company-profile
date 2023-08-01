8cb5b1d9b0fb62c88aad30a544462ad99d10dd61
chuckiedonald <101234522+chuckiedonald@users.noreply.github.com>
2023-08-01 17:40:21 +0100

    Update skillAnimat.js
    new skill animation

--- public/javascript/skillAnimat.js
+++ public/javascript/skillAnimat.js
@@ -1,41 +1,40 @@
-
 function increaseValueOnScroll() {
-    const skillBars = document.querySelectorAll('.skill-bar');
-    const duration = 3000; // 2 seconds
-  
-    function increaseValue(skillBar, targetValue, targetDivClass) {
-      let currentValue = 0;
-      const targetDivs = document.querySelectorAll(".ch-wdt");
-  
-      const interval = setInterval(() => {
-        if (currentValue <= targetValue) {
-          skillBar.textContent = currentValue;
-          targetDivs.forEach((div) => {
-            div.style.width = currentValue + '%';
-          });
-          currentValue++;
-        } else {
-          clearInterval(interval);
-        }
-      }, duration / targetValue);
-    }
-  
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const targetValue = parseInt(entry.target.textContent);
-          const targetDivClass = entry.target.dataset.target;
-          increaseValue(entry.target, targetValue, targetDivClass);
-          observer.unobserve(entry.target);
-        }
-      });
-    });
-  
-    skillBars.forEach((skillBar) => {
-      observer.observe(skillBar);
-    });
+  const skillBars = document.querySelectorAll('.skill-bar');
+  const duration = 2000; // 2 seconds
+
+  function increaseValue(skillBar, targetValue, targetDivClass) {
+    let currentValue = 0;
+    const targetDivs = document.querySelectorAll(`.${targetDivClass}`);
+
+    const interval = setInterval(() => {
+      if (currentValue <= targetValue) {
+        skillBar.textContent = currentValue;
+        targetDivs.forEach((div) => {
+          div.style.width = currentValue + '%';
+        });
+        currentValue++;
+      } else {
+        clearInterval(interval);
+      }
+    }, duration / targetValue);
   }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    increaseValueOnScroll();
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const targetValue = parseInt(entry.target.textContent);
+        const targetDivClass = entry.target.dataset.target;
+        increaseValue(entry.target, targetValue, targetDivClass);
+        observer.unobserve(entry.target);
+      }
+    });
   });
+
+  for (let i = 0; i < skillBars.length; i++) {
+    observer.observe(skillBars[i]);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  increaseValueOnScroll();
+});
